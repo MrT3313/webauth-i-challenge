5152eaf5f0e041202e6a1319689cb7e3a6654ead
MrT3313 <rturgeon13@gmail.com>
2019-06-04 13:32:32 -0500

    MIGRATIONS
    USERS table

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/webAUTH_1_challenge.db3'
+    }
+  },
+  useNullAsDefault: true,
+
+};
