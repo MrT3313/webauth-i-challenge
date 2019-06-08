1bcebf9f07a86d50a335331bd01b0baf8e892e7a
MrT3313 <rturgeon13@gmail.com>
2019-06-08 12:13:49 -0500

    login working

--- api/routers/loginRouter.js
+++ api/routers/loginRouter.js
@@ -1,5 +1,6 @@
 // IMPORTS
 const express = require('express')
+const bcrypt = require('bcrypt')
 
 // KNEX version of DB
     const DB_KNEX = require('../../data/dbConfig.js')
@@ -16,7 +17,29 @@ const express = require('express')
 
 // - POST - //
     router.post('/', async (req,res) => {
-        
+        console.log('loginRouter POST/')
+        const { userName, password } = req.body
+        console.log(userName, password)
+
+        DB_KNEX('USERS')
+            .where('userName', userName)
+                .first()
+                .then( user => {
+                    console.log('user', user)
+
+                    const pwVerification = bcrypt.compareSync(password, user.password)
+                        console.log(pwVerification)
+
+                    if (user && pwVerification) {
+                        res.status(200).json({ message: `Welcome ${userName}`})
+                    } else {
+                        res.status(500).json({ message: `Invalid Credentials`})
+                    }
+
+                })
+                .catch( () => {
+                    res.status(500).json({ message: "yo shit broken"})
+                })
     })
 
 // EXPORTS
